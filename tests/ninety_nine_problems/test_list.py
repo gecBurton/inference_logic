4c9d7507644c82604b4edfb3639db488f307e272
gecBurton <g.e.c.burton@gmail.com>
2020-10-13 14:25:37 +0100

    added test for 09

--- tests/ninety_nine_problems/test_list.py
+++ tests/ninety_nine_problems/test_list.py
@@ -166,3 +166,44 @@ def test_eliminate_consecutive_duplicates_of_list_elements_08():
     query = dict(compress=[1, 2, 1], list=Q)
     out = list(search(db, query))
     assert out == [{Q: [1, 2, 1]}]
+
+
+def test_pack_consecutive_duplicates_of_list_elements_into_sublists_09():
+    """
+    % P09 (**):  Pack consecutive duplicates of list elements into sublists.
+
+    % pack(L1,L2) :- the list L2 is obtained from the list L1 by packing
+    %    repeated occurrences of elements into separate sublists.
+    %    (list,list) (+,?)
+
+    pack([],[]).
+    pack([X|Xs],[Z|Zs]) :- transfer(X,Xs,Ys,Z), pack(Ys,Zs).
+
+    % transfer(X,Xs,Ys,Z) Ys is the list that remains from the list Xs
+    %    when all leading copies of X are removed and transfered to Z
+
+    transfer(X,[],[],[X]).
+    transfer(X,[Y|Ys],[Y|Ys],[X]) :- X \\= Y.
+    transfer(X,[X|Xs],Ys,[X|Zs]) :- transfer(X,Xs,Ys,Zs).
+    """
+
+    Xs, Ys, Zs = Variable.factory("Xs", "Ys", "Zs")
+    db = [
+        dict(pack=[], list=[]),
+        Rule(
+            dict(pack=[X, *Xs], list=[Z, *Zs]),
+            dict(transfer=X, a=Xs, b=Ys, c=Z),
+            dict(pack=Ys, list=Zs),
+        ),
+        dict(transfer=X, a=[], b=[], c=[X]),
+        Rule(
+            dict(transfer=[X], a=[Y, *Ys], b=[Y, *Ys], c=[X]),
+            Assert(lambda X, Y: X != Y),
+        ),
+        Rule(
+            dict(transfer=X, a=[X, *Xs], b=Ys, c=[X, *Zs]),
+            dict(transfer=X, a=Xs, b=Ys, c=Zs),
+        ),
+    ]
+    query = dict(pack=[1, 1], list=Q)
+    assert list(search(db, query)) == [{Q: [[1, 1]]}]
