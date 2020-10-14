9d7785cf423a76ddc8d993395b04ba291846268a
gecBurton <g.e.c.burton@gmail.com>
2020-10-14 06:51:02 +0100

    test_33 passing

--- tests/ninety_nine_problems/test_arithmetic.py
+++ tests/ninety_nine_problems/test_arithmetic.py
@@ -30,3 +30,30 @@ def test_determine_the_greatest_common_divisor_of_two_positive_integer_numbers_3
         ),
     ]
     assert list(search(db, dict(a=36, b=63, gcd=Q))) == [{Q: 9}]
+
+
+def test_determine_whether_two_positive_integer_numbers_are_coprime_33():
+    """
+    % P33 (*) Determine whether two positive integer numbers are coprime.
+    %     Two numbers are coprime if their greatest common divisor equals 1.
+
+    % coprime(X,Y) :- X and Y are coprime.
+    %    (integer, integer) (+,+)
+
+    :- ensure_loaded(p32).
+
+    coprime(X,Y) :- gcd(X,Y,1).
+    """
+    X, Y, Z, G, Q = Variable.factory("X", "Y", "Z", "G", "Q")
+    db = [
+        Rule(dict(a=X, b=0, gcd=X), Assert(lambda X: X > 0)),
+        Rule(
+            dict(a=X, b=Y, gcd=G),
+            Assert(lambda Y: Y > 0),
+            Assign(Z, lambda X, Y: X % Y),
+            dict(a=Y, b=Z, gcd=G),
+        ),
+        Rule(dict(a=X, b=Y, coprime=True), dict(a=X, b=Y, gcd=1)),
+    ]
+    query = dict(a=15, b=4, coprime=Q)
+    assert list(search(db, query)) == [{Q: True}]
