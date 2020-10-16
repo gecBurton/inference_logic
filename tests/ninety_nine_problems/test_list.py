f45adc9803a1839133f69872060c3868ce3e0221
gecBurton <g.e.c.burton@gmail.com>
2020-10-16 13:12:30 +0100

    test 13 failing

--- tests/ninety_nine_problems/test_list.py
+++ tests/ninety_nine_problems/test_list.py
@@ -392,7 +392,59 @@ def test_12():
 
 @pytest.mark.xfail
 def test_13():
-    assert False
+    r"""
+    P13 (**): Run-length encoding of a list (direct solution)
+
+    encode_direct(L1,L2) :- the list L2 is obtained from the list L1 by
+       run-length encoding. Consecutive duplicates of elements are encoded
+       as terms [N,E], where N is the number of duplicates of the element E.
+       However, if N equals 1 then the element is simply copied into the
+       output list.
+       (list,list) (+,?)
+
+    encode_direct([],[]).
+    encode_direct([X|Xs],[Z|Zs]) :- count(X,Xs,Ys,1,Z), encode_direct(Ys,Zs).
+
+    count(X,Xs,Ys,K,T) Ys is the list that remains from the list Xs
+       when all leading copies of X are removed. T is the term [N,X],
+       where N is K plus the number of X's that can be removed from Xs.
+       In the case of N=1, T is X, instead of the term [1,X].
+
+    count(X,[],[],1,X).
+    count(X,[],[],N,[N,X]) :- N > 1.
+    count(X,[Y|Ys],[Y|Ys],1,X) :- X \= Y.
+    count(X,[Y|Ys],[Y|Ys],N,[N,X]) :- N > 1, X \= Y.
+    count(X,[X|Xs],Ys,K,T) :- K1 is K + 1, count(X,Xs,Ys,K1,T).
+
+    """
+    Xs, Ys, Zs = Variable.factory("Xs", "Ys", "Zs")
+    N, T, K, K1 = Variable.factory("N", "T", "K", "K1")
+
+    db = [
+        dict(encode_direct=[], list=[]),
+        Rule(
+            dict(encode_direct=[X, *Xs], list=[Z, *Zs]),
+            dict(count=X, a=Xs, b=Ys, c=1, d=Z),
+            dict(encode_direct=Ys, list=Zs),
+        ),
+        dict(count=X, a=[], b=[], c=1, d=X),
+        Rule(dict(count=X, a=[], b=[], c=N, d=[N, X]), Assert(lambda N: N > 1)),
+        Rule(
+            dict(count=X, a=[Y, *Ys], b=[Y, *Ys], c=1, d=X), Assert(lambda X, Y: X != Y)
+        ),
+        Rule(
+            dict(count=X, a=[Y, *Ys], b=[Y, *Ys], c=N, d=[N, X]),
+            Assert(lambda N: N > 1),
+            Assert(lambda X, Y: X != Y),
+        ),
+        Rule(
+            dict(count=X, a=[X, *Xs], b=Ys, c=K, d=T),
+            Assign(K1, lambda K: K + 1),
+            dict(coun=X, a=Xs, b=Ys, c=K1, d=T),
+        ),
+    ]
+    query = dict(encode_direct=[1, 2, 2, 3, 3, 3], list=Q)
+    assert list(search(db, query)) == [{Q: [1, [2, 2], [3, 3]]}]
 
 
 def test_14():
