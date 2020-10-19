2f32b65b05be49751eaf8c8f21a155f1769f3591
gecBurton <g.e.c.burton@gmail.com>
2020-10-19 13:27:06 +0100

    bumped min py version

--- setup.py
+++ setup.py
@@ -23,16 +23,13 @@ test_requirements = [
 setup(
     author="George EC Burton",
     author_email="g.e.c.burton@gmail.com",
-    python_requires=">=3.5",
+    python_requires=">=3.8",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
     description="declarative programming on json-like objects in Python",
